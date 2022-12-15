2d11153c0db22d738d9e0e54e105c7f53e218864
Bastián Rodríguez <bastrodriguezr@Bastians-MacBook-Air.local>
2022-12-15 14:14:08 -0300

    header fixed

--- src/app/pages/nuevo-viaje/nuevo-viaje.module.ts
+++ src/app/pages/nuevo-viaje/nuevo-viaje.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { NuevoViajePageRoutingModule } from './nuevo-viaje-routing.module';
 
 import { NuevoViajePage } from './nuevo-viaje.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NuevoViajePageRoutingModule
+    NuevoViajePageRoutingModule,
+    ComponentsModule,
   ],
   declarations: [NuevoViajePage]
 })
--- src/app/pages/nuevo-viaje/nuevo-viaje.page.ts
+++ src/app/pages/nuevo-viaje/nuevo-viaje.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoViajePage implements OnInit {
 
+  pageTitle = 'Nuevo Viaje';
+  isNotHome = true;
+  
   constructor() { }
 
   ngOnInit() {
