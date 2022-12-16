afb2cdd97a2db9611af2191592221dca3975a0b3
odakasam <87153833+odakasam@users.noreply.github.com>
2022-12-16 01:43:48 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { providePerformance,getPerformance } from '@angular/fire/performance';
 import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 import { AuthGuard } from '@angular/fire/auth-guard';
 import { AuthService } from './services/auth.service';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -58,6 +59,8 @@ import { AuthService } from './services/auth.service';
     UserTrackingService,
     AuthGuard,
     AuthService,
+    EmailComposer,
+    
   ],
   bootstrap: [AppComponent],
 })
